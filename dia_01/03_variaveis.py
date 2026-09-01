921a0b2992e0802d2128cd486cbe84da3eac8d6b
Gustavo <gustavopolicarpo111@gmail.com>
2026-08-31 21:33:02 -0300

    Variáveis

---
+++ dia_01/03_variaveis.py
@@ -0,0 +1,17 @@
+nome = "Gus Souza"
+print(nome)
+
+nome = "João da Silva"
+print(nome)
+
+soma = 10 + 5
+print("10 + 5 =", soma)
+
+resultado = 10 - 5 
+print("10 - 5 =", resultado)
+
+resultado = 10 * 5
+print("10 * 5 =", resultado)
+
+resultado = resultado +10
+print (resultado)
