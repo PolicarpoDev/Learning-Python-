aac4bc7dae99f05a8e1102dd0be5e9577cccc260
Gustavo <gustavopolicarpo111@gmail.com>
2026-09-08 16:48:27 -0300

    Atualiza exercício da sorveteria

--- exercícios/08_sorveteria.py
+++ exercícios/08_sorveteria.py
@@ -1,61 +1,43 @@
 print("=== SORVETERIA ===")
 
-print("\nEscolha o tipo de sorvete:")
-print("1 - Casquinha - R$ 1,00")
+print("\n1 - Casquinha - R$ 1,00")
 print("2 - Cascão - R$ 2,50")
 print("3 - Cestinha - R$ 4,00")
 
-tipo = int(input("Digite a opção: "))
+tipo = int(input("Escolha o tipo de sorvete: "))
 
 if tipo == 1:
-    valor_sorvete = 1.00
+    valor = 1.00
 elif tipo == 2:
-    valor_sorvete = 2.50
+    valor = 2.50
 elif tipo == 3:
-    valor_sorvete = 4.00
+    valor = 4.00
 else:
-    valor_sorvete = 0
     print("Opção inválida!")
+    valor = 0
 
-print("\nEscolha o sabor:")
-print("1 - Morango")
+
+print("\n1 - Morango")
 print("2 - Creme")
 print("3 - Chocolate")
 
-sabor = int(input("Digite a opção: "))
+sabor = int(input("Escolha o sabor: "))
 
-if sabor == 1:
-    nome_sabor = "Morango"
-elif sabor == 2:
-    nome_sabor = "Creme"
-elif sabor == 3:
-    nome_sabor = "Chocolate"
-else:
-    nome_sabor = "Inválido"
+if sabor < 1 or sabor > 3:
     print("Sabor inválido!")
 
-print("\nEscolha a cobertura:")
-print("1 - Caramelo - R$ 1,50")
+
+print("\n1 - Caramelo - R$ 1,50")
 print("2 - Morango - R$ 1,50")
 print("3 - Chocolate - R$ 1,50")
-print("4 - Sem cobertura - R$ 0,00")
+print("4 - Sem cobertura")
 
-cobertura = int(input("Digite a opção: "))
+cobertura = int(input("Escolha a cobertura: "))
 
-if cobertura == 1:
-    valor_cobertura = 1.50
-elif cobertura == 2:
-    valor_cobertura = 1.50
-elif cobertura == 3:
-    valor_cobertura = 1.50
-elif cobertura == 4:
-    valor_cobertura = 0.00
-else:
-    valor_cobertura = 0
+if cobertura >= 1 and cobertura <= 3:
+    valor += 1.50
+elif cobertura != 4:
     print("Cobertura inválida!")
 
-total = valor_sorvete + valor_cobertura
 
-print("\n=== PEDIDO ===")
-print("Sabor:", nome_sabor)
-print(f"Valor a ser pago: R$ {total:.2f}")
+print(f"\nValor a pagar: R$ {valor:.2f}")
