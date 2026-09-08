10373e79c1cf515467040cf642c5e996d4e19948
Gustavo <gustavopolicarpo111@gmail.com>
2026-09-08 18:07:58 -0300

    Exercícios

---
+++ exercícios/frase.py
@@ -0,0 +1,24 @@
+
+frases = {}
+
+while True:
+    frase = input("Entre com a frase: ")
+    if frase == "":
+        break
+
+    if frase not in frases:
+        frases[frase] = 1
+    else: 
+        frases[frase] += 1
+
+for i in frases:
+    print(i,"->", frases[i])
+
+# for i, j in frases.items():
+# print(i, "->", j)
+
+items = list(frases.items())
+items.sort(key = lambda x: x[-1], reverse=True)
+
+for i, j in items:
+    print(i, "->", j)
